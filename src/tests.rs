b0a008b47cd6992fbe276055cd63e1fd258d6a65
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2023-09-13 17:58:18 +0200

    fmt: cargo fmt

--- src/tests.rs
+++ src/tests.rs
@@ -7,7 +7,7 @@ mod serde_support {
     use serde_json::Value;
     use uuid::Uuid;
 
-    use crate::{Chat, VERSION_1_8, ClickEvent, HoverEvent, EntityTooltip, TranslationComponent};
+    use crate::{Chat, ClickEvent, EntityTooltip, HoverEvent, TranslationComponent, VERSION_1_8};
 
     #[test]
     pub fn chat_serialize() {
@@ -22,11 +22,20 @@ mod serde_support {
 
     #[test]
     pub fn standard_chat() {
-        let chat_orig = Chat::component(TranslationComponent::new("chat.type.text")
-            .argument(Chat::text("Herobrine").insertion(Some("Herobrine"))
-                .click(Some(ClickEvent::suggest("/msg Herobrine ")))
-                .hover(Some(HoverEvent::ShowEntity(EntityTooltip::new(Some(Chat::text("Herobrine")), Option::<&str>::None, Some(Uuid::from_str("f84c6a79-0a4e-45e0-879b-cd49ebd4c4e2").unwrap()))))))
-            .argument(Chat::text("I don't exist")));
+        let chat_orig = Chat::component(
+            TranslationComponent::new("chat.type.text")
+                .argument(
+                    Chat::text("Herobrine")
+                        .insertion(Some("Herobrine"))
+                        .click(Some(ClickEvent::suggest("/msg Herobrine ")))
+                        .hover(Some(HoverEvent::ShowEntity(EntityTooltip::new(
+                            Some(Chat::text("Herobrine")),
+                            Option::<&str>::None,
+                            Some(Uuid::from_str("f84c6a79-0a4e-45e0-879b-cd49ebd4c4e2").unwrap()),
+                        )))),
+                )
+                .argument(Chat::text("I don't exist")),
+        );
 
         let serialized_str = r#"{"translate":"chat.type.text","with":[{"text":"Herobrine","clickEvent":{"action":"suggest_command","value":"/msg Herobrine "},"hoverEvent":{"action":"show_entity","value":"{id:f84c6a79-0a4e-45e0-879b-cd49ebd4c4e2,name:Herobrine}"},"insertion":"Herobrine"},{"text":"I don't exist"}]}"#;
         let chat: Chat = serde_json::from_str(serialized_str).unwrap();
